146daadfeb2fdd0fe0d7daef611467ff72aa74b3
Graham Cottridge <codinggc@gmail.com>
2018-09-06 20:04:00 +0100

    Fix issue with gravatar images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def avatar_url user
     return user.image if user.image
-    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-    "http://wwww.gravatar.com/avatar/#{gravatar_id}.jpg"
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    avatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
   end
 end
